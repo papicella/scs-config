b6301600eca730525f918f332814efd23ea28d7a
papicella <pas.apicella@tpg.com.au>
2018-11-28 12:52:41 +1100

    Initial commit

---
+++ src/main/java/com/example/scsconfig/ScsConfigApplication.java
@@ -0,0 +1,14 @@
+package com.example.scsconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ScsConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScsConfigApplication.class, args);
+    }
+}
